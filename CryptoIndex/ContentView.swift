268e71f9c162532b667299439bd9b88a5882afd4
RAM6IOS <ios191169@gmail.comm>
2022-05-08 17:04:50 +0100

    Initial Commit

---
+++ CryptoIndex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoIndex
+//
+//  Created by Bouchedoub Rmazi on 8/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoIndex/CryptoIndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoIndexApp.swift
+//  CryptoIndex
+//
+//  Created by Bouchedoub Rmazi on 8/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
